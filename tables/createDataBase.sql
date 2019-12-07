e4e3560e7f8703d5abc238d0bc15edaeb6e95114
Will Carrara <43623732+Will-Carrara@users.noreply.github.com>
2019-12-07 10:52:37 -0800

    Update createDataBase.sql

--- tables/createDataBase.sql
+++ tables/createDataBase.sql
@@ -26,20 +26,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `destination` (
-  `id` int(11) NOT NULL,
+  `destinationId` int(11) NOT NULL,
   `destination` varchar(25) NOT NULL,
   `engine_num` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `engineReq` int(11) NOT NULL,
+  PRIMARY KEY (`destinationId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `destination`
 --
 
-INSERT INTO `destination` (`id`, `destination`, `engine_num`) VALUES
-(1, 'orbital transfer', 3),
-(2, 'low earth orbit', 5),
-(3, 'orbital transfer', 25);
+INSERT INTO `destination` (`destinationId`, `destination`, `engine_num`, `engineReq`) VALUES
+(1, 'orbital transfer', 3, 5),
+(2, 'low earth orbit', 5, 6),
+(3, 'orbital transfer', 25, 7);
 
 -- phpMyAdmin SQL Dump
 -- version 4.0.10deb1
@@ -53,7 +54,6 @@ INSERT INTO `destination` (`id`, `destination`, `engine_num`) VALUES
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
 --
 -- Database: `final`
 --
@@ -65,40 +65,41 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `engine` (
-  `id` int(11) NOT NULL,
+  `engineId` int(11) NOT NULL,
   `upgrade` varchar(50) NOT NULL,
-  `cost` int(11) NOT NULL,
+  `engineCost` int(11) NOT NULL,
   `description` varchar(50) NOT NULL,
   `weight` int(11) NOT NULL,
   `material` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
+  `payloadReq` int(11) NOT NULL,
+  PRIMARY KEY (`engineId`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `engine`
 --
 
-INSERT INTO `engine` (`id`, `upgrade`, `cost`, `description`, `weight`, `material`) VALUES
-(1, 'turbopump', 30000, 'See part manual 11d', 500, 'steel'),
-(2, 'oxidizer preburner', 12000, 'See part manual 15a', 600, 'steel'),
-(3, 'lox bleed valve', 3000, 'See part manual 14b', 50, 'aluminum'),
-(4, 'low-pressure fuel duct', 7000, 'See part manual 14d', 400, 'steel'),
-(5, 'chamber coolant valve', 9000, 'See part manual 17a', 70, 'misc'),
-(6, 'ahm controller', 15000, 'See part manual 19c', 15, 'misc'),
-(7, 'dome injector', 4000, 'See part manual 19a', 70, 'steel'),
-(8, 'turbine drive duct', 20000, 'See part manual 17a', 150, 'composite'),
-(9, 'gimble bearing', 2000, 'See part manual 13d', 10, 'ceramic'),
-(10, 'composite thrust chamber', 25000, 'See part manual 14b', 300, 'composite'),
-(11, 'tvc actuator',5000, 'See part manual 17d',25, 'misc'),
-(12, 'dual exhaust manifold', 16000, 'See part manual 17c', 150, 'composite'),
-(13, 'gimble outtrigger', 1000, 'See part manual 17a', 20, 'composite'),
-(14, 'prefill check valve', 1500, 'See part manual 11a', 50, 'aluminum'),
-(15, 'tops mounting pad', 4000, 'See part manual 12d', 60, 'misc'),
-(16, 'redundant fuel duct', 17000, 'See part manual 13a', 150, 'composite'),
-(17, 'redundant fuel inlet', 8000, 'See part manual 18d', 50, 'aluminum'),
-(18, 'turbine discharge duct', 4000, 'See part manual 19b', 80, 'aluminum'),
-(19, 'buner caps', 1000, 'See part manual 19a', 10, 'misc'),
-(20, 'bellow covers', 3000, 'See part manual 11b', 50, 'misc');
+INSERT INTO `engine` (`engineId`, `upgrade`, `engineCost`, `description`, `weight`, `material`, `payloadReq`) VALUES
+(1, 'turbopump', 30000, 'See part manual 11d', 500, 'steel', 1),
+(2, 'oxidizer preburner', 12000, 'See part manual 15a', 600, 'steel', 2),
+(3, 'lox bleed valve', 3000, 'See part manual 14b', 50, 'aluminum', 3),
+(4, 'low-pressure fuel duct', 7000, 'See part manual 14d', 400, 'steel', 4),
+(5, 'chamber coolant valve', 9000, 'See part manual 17a', 70, 'misc', 1),
+(6, 'ahm controller', 15000, 'See part manual 19c', 15, 'misc', 2),
+(7, 'dome injector', 4000, 'See part manual 19a', 70, 'steel', 3),
+(8, 'turbine drive duct', 20000, 'See part manual 17a', 150, 'composite', 4),
+(9, 'gimble bearing', 2000, 'See part manual 13d', 10, 'ceramic', 1),
+(10, 'composite thrust chamber', 25000, 'See part manual 14b', 300, 'composite', 2),
+(11, 'tvc actuator',5000, 'See part manual 17d',25, 'misc', 2),
+(12, 'dual exhaust manifold', 16000, 'See part manual 17c', 150, 'composite', 3),
+(13, 'gimble outtrigger', 1000, 'See part manual 17a', 20, 'composite', 4),
+(14, 'prefill check valve', 1500, 'See part manual 11a', 50, 'aluminum', 1),
+(15, 'tops mounting pad', 4000, 'See part manual 12d', 60, 'misc', 2),
+(16, 'redundant fuel duct', 17000, 'See part manual 13a', 150, 'composite', 3),
+(17, 'redundant fuel inlet', 8000, 'See part manual 18d', 50, 'aluminum', 4),
+(18, 'turbine discharge duct', 4000, 'See part manual 19b', 80, 'aluminum', 1),
+(19, 'buner caps', 1000, 'See part manual 19a', 10, 'misc', 2),
+(20, 'bellow covers', 3000, 'See part manual 11b', 50, 'misc', 3);
 
 --
 -- Database: `final`
@@ -114,21 +115,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `payload` (
-  `id` int(11) NOT NULL,
+  `payloadId` int(11) NOT NULL,
   `payload` int(11) NOT NULL,
-  `cost` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `payloadCost` int(11) NOT NULL,
+  `engineReq` int(11) NOT NULL,
+  PRIMARY KEY (`payloadId`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `payload`
 --
 
-INSERT INTO `payload` (`id`, `payload`, `cost`) VALUES
-(1, 100000, 50000000),
-(2, 75000, 40000000),
-(3, 50000, 30000000),
-(4, 25000, 20000000);
-
-
-
+INSERT INTO `payload` (`payloadId`, `payload`, `payloadCost`, `engineReq`) VALUES
+(1, 100000, 50000000, 5),
+(2, 75000, 40000000, 6),
+(3, 50000, 30000000, 7),
+(4, 25000, 20000000, 8);
